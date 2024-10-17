7960f1d5a82352d9b7d17845da44b7dc9f2eb663
keijiek <kumatan800@gmail.com>
2024-10-18 08:03:09 +0900

    20241018_dev

--- view/pages/parts/_DrawerMenuSet.class.php
+++ view/pages/parts/_DrawerMenuSet.class.php
@@ -13,8 +13,13 @@ class DrawerButtonSet
   function button()
   {
 ?>
-    <button type="button" id="js_drawerButton" class="bl_drawerButton z-10">
-      Button
+    <button type="button" id="js_drawerButton" class="bl_drawerButton flex lg:hidden flex-col fixed z-10 right-0 bg-lime-800 text-white  w-16 rounded-bl-lg">
+      <div class="bg-inherit w-full grid place-items-center ">
+        <svg class="w-[48px] h-[48px] text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
+        </svg>
+      </div>
+      <div class="grid place-items-center w-full -translate-y-2">menu</div>
     </button>
   <?php
   }
--- view/pages/parts/_Header.class.php
+++ view/pages/parts/_Header.class.php
@@ -8,7 +8,7 @@ class Header
   {
 ?>
     <header class="px-4">
-      <div class="container mx-auto px-2 lg:px-0">
+      <div class="container mx-auto px-2 lg:px-0 pt-4">
         <h1 class="text-2xl md:text-3xl text-amber-950 font-bold">
           <a href="<?php echo home_url() ?>"><?php echo bloginfo('name') ?></a>
         </h1>
@@ -19,14 +19,14 @@ class Header
             foreach (NAV_MENU_ITEMS as $item) {
             ?>
               <li class="flex gap-2 align-middle justify-start">
-                <a href="<?= $item->href ?>" class="text-lg">
+                <a href="<?= $item->href ?>" class="text-lg flex items-center justify-start gap-1">
                   <img
                     src="<?= $item->icon ?>"
                     width="36"
                     height="36"
-                    class="inline"
+                    class="block"
                     alt="" />
-                  <span><?= $item->title ?></span>
+                  <span class="leading-none"><?= $item->title ?></span>
                 </a>
               </li>
             <?php
